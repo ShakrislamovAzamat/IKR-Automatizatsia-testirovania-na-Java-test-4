8dff30d0343e8d5023fbe7bca8b88f47fd2766c3
Azamat <sch_azamat@mail.ru>
2024-06-08 12:15:12 +0500

    Ready

--- src/test/java/com/max/UITest.java
+++ src/test/java/com/max/UITest.java
@@ -1,52 +1,29 @@
 package com.max;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.concurrent.TimeUnit;
 
-public class UITest {
+public class UITest extends UIAbstractTest{
 
     @Test
     void testGBNotEmailLogin() {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--incognito");
-        //options.addArguments("--headless");
-        options.addArguments("start-maximized");
-        options.addArguments("--remote-allow-origins=*");
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        driver.get("https://gb.ru/login");
 
-        driver.findElement(By.xpath("/html/body/div[2]/div[7]/div/form/div[1]/input")).sendKeys("login");
-        driver.findElement(By.xpath("/html/body/div[2]/div[7]/div/form/div[2]/input")).sendKeys("password");
-        driver.findElement(By.xpath("/html/body/div[2]/div[7]/div/form/div[4]/input")).click();
 
-        Assertions.assertFalse(driver.findElements(By.xpath("/html/body/div[2]/div[7]/div/form/div[1]/ul")).isEmpty());
+        loginPage.inputEmail("login");
+        loginPage.inputPassword("password");
+        loginPage.clickLoginButton();
+
+        Assertions.assertFalse(loginPage.getNotLoginError().isEmpty());
+
     }
 
     @Test
     void testGBWithoutPassword() {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--incognito");
-        //options.addArguments("--headless");
-        options.addArguments("start-maximized");
-        options.addArguments("--remote-allow-origins=*");
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        driver.get("https://gb.ru/login");
-        driver.findElement(By.xpath("/html/body/div[2]/div[7]/div/form/div[1]/input")).sendKeys("login@login.ru");
-        driver.findElement(By.xpath("/html/body/div[2]/div[7]/div/form/div[4]/input")).click();
 
-        Assertions.assertFalse(driver.findElements(By.xpath("/html/body/div[2]/div[7]/div/form/div[2]/ul")).isEmpty());
-
+        loginPage.inputEmail("login@login.ru");
+        loginPage.clickLoginButton();
     }
 
 }
